e4f13a6f9847605dda76eee409fff1d5fe882f21
ASUS <ASUS@DESKTOP-3RT5GIB>
2018-08-02 08:23:37 +0700

    For while - Do while 2

---
+++ src/j08/contoh2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author ASUS
+ */
+public class contoh2 {
+    public static void main(String[] args) 
+    {
+    int x;
+    for(x = 1; x <= 10; x++)
+    {
+        System.out.print(x + " ");
+    }
+    
+ 
+    while(x <= 10)
+    {
+        System.out.print(x + " ");
+        x++;
+    }
+    
+
+    do
+    {
+        System.out.print(x + " ");
+        x++;
+    }
+    while(x <= 10);
+    
+    }
+}
+    
+
